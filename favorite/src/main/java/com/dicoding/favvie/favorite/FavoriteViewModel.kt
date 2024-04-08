6d87d82412e0b7c16dc08fab9bf57637a270b61e
ptrdyp <alviany.prameswari@gmail.com>
2024-04-08 14:07:24 +0700

    Delete unused import

--- favorite/src/main/java/com/dicoding/favvie/favorite/FavoriteViewModel.kt
+++ favorite/src/main/java/com/dicoding/favvie/favorite/FavoriteViewModel.kt
@@ -1,11 +1,7 @@
 package com.dicoding.favvie.favorite
 
-import android.app.Application
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
-import com.dicoding.favvie.core.data.remote.RemoteDataSource
 import com.dicoding.favvie.core.domain.usecase.MovieUseCase
 
 class FavoriteViewModel(movieUseCase: MovieUseCase): ViewModel() {

941e2745fd4a9e7bc97741ea8504b08c76d512b5
ptrdyp <alviany.prameswari@gmail.com>
2024-04-11 20:12:47 +0700

    edit build.gradle.kts favorite(2)

--- favorite/build.gradle.kts
+++ favorite/build.gradle.kts
@@ -46,11 +46,9 @@ dependencies {
     implementation (project(":core"))
     implementation (project(":app"))
 
+    implementation("androidx.annotation:annotation:1.7.1")
     implementation("androidx.core:core-ktx:1.12.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation("androidx.annotation:annotation:1.7.1") {
-        exclude(group = "androidx.annotation", module = "annotation")
-    }
 }
